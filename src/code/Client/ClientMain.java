b206d5d7d97789e898be920e20f063aa05d6828b
dev-danial <d_alihosseiny@yahoo.com>
2016-06-30 18:54:23 +0430

    repackaging

--- src/code/Client/ClientMain.java
+++ src/code/Client/ClientMain.java
@@ -1,10 +1,11 @@
-package code;
+package code.Client;
 
 import gui.LoginPage;
 
 import java.io.IOException;
 
-public class Main {
+public class ClientMain
+{
 
     public static void main(String[] args) throws IOException {
         new LoginPage();
---
+++ src/code/Server/ServerMain.java
@@ -0,0 +1,9 @@
+package code.Server;
+
+public class ServerMain
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
