df949bb4cc2a6f9ba84bf2d16b40dd9d1f88659d
dev-danial <d_alihosseiny@yahoo.com>
2016-07-02 00:32:47 +0430

    minor changes

--- src/code/PrimitiveClasses/Group.java
+++ src/code/PrimitiveClasses/Group.java
@@ -1,16 +1,8 @@
 package code.PrimitiveClasses;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-
-public class Group extends BasicChat implements JsonParsable
+public class Group extends BasicChat
 {
     private User[] memmbers;
-
-    public Group()
-    {
-        super();
-    }
 
     public Group(String title)
     {
@@ -23,28 +15,5 @@ public class Group extends BasicChat implements JsonParsable
 
     public void setMemmbers(User[] memmbers) {
         this.memmbers = memmbers;
-    }
-
-    private User[] memmbers;
-    public Group(String title)
-    {
-        super(title);
-    }
-
-    @Override
-    public JSONObject toJsonObj() {
-        return null;
-    }
-
-    @Override
-    public void parsJsonObj(JSONObject obj) {
-        Group group = new Group(obj.get("title").toString());
-            JSONArray membersarray = (JSONArray) obj.get("members");
-        User[] thismembers = new User[membersarray.size()];
-        for(int i=0; i<membersarray.size();i++){
-            thismembers[i].setUsername(membersarray.get(i).toString());
-        }
-            group.setMemmbers(thismembers);
-
     }
 }
