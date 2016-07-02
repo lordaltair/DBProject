8dc6f91035c1201a81fc187dc37cc24ffb73720b
Ali <a.hoseinmardy@gmail.com>
2016-07-02 17:37:02 +0800

    Merge branch 'master' of https://github.com/lordaltair/DBProject
    # Conflicts:
    #	src/gui/LoginPage.java

---
+++ src/gui/Signup.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Ali on 7/2/2016.
+ */
+public class Signup {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextArea textArea1;
+    private JTextField textField5;
+    private JButton signUpButton;
+}
