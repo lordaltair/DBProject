c0be3adeb9050defb4825cb311923a31638192ee
Ali <a.hoseinmardy@gmail.com>
2016-07-02 17:12:33 +0800

    Merge branch 'master' of https://github.com/lordaltair/DBProject
    # Conflicts:
    #	src/gui/LoginPage.java

---
+++ src/gui/ProfilePage.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Ali on 7/2/2016.
+ */
+public class ProfilePage {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextArea textArea1;
+    private JTextField textField5;
+    private JButton createChannelButton;
+    private JButton createGroupButton;
+    private JButton updateProfileButton;
+}
---
+++ src/gui/SearchPage.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Ali on 7/2/2016.
+ */
+public class SearchPage {
+    private JPanel LoginPanel;
+    private JTextField textField1;
+    private JButton searchHastagButton;
+    private JButton searchNameButton;
+    private JTextField textField2;
+    private JButton addButton;
+    private JButton sendUnknownMessageButton;
+    private JTree tree1;
+}
